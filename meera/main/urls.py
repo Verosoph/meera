8b1be0ea6ecf191807f8feb11f506d090a8c8332
Marco Mettke <mettkemarco@gmail.com>
2018-03-19 15:24:58 +0100

    first content inside

--- meera/main/urls.py
+++ meera/main/urls.py
@@ -5,6 +5,7 @@ from .import views
 urlpatterns = [
     url(r'^$',views.index),
     url(r'^t1',views.thema1),
+    url(r'^t1_1',views.thema1_1),
     url(r'^t2',views.thema2),
     url(r'^t3',views.thema3),
     url(r'^t4',views.thema4),
--- meera/main/views.py
+++ meera/main/views.py
@@ -10,6 +10,10 @@ def thema1(request):
     today = datetime.datetime.now()
     return render(request, 'thema1.html', {'today':today})
 
+def thema1_1(request):
+    today = datetime.datetime.now()
+    return render(request, 'thema1_1.html', {'today':today})
+
 def thema2(request):
     today = datetime.datetime.now()
     return render(request, 'thema2.html', {'today':today})
